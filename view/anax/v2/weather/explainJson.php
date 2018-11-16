08d4d1763a35cc0fa25ef9beb65d21c6006c2b5b
Martin <martin.d@live.se>
2018-11-16 22:52:09 +0000

    updated view

--- view/anax/v2/weather/explainJson.php
+++ view/anax/v2/weather/explainJson.php
@@ -6,7 +6,7 @@ namespace Anax\View;
  * Template file to render a view.
  */
 
-// Show incoming variables and view helper functions
+// Show incoming variables and view helper functions,
 /* echo showEnvironment(get_defined_vars(), get_defined_functions()); */
 
 function getMyIp()
