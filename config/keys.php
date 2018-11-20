c1bf8ad00e3390a762f89ad59b423d3a97530d59
Martin <martin.d@live.se>
2018-11-20 20:21:15 +0000

    updated config file

--- config/keys.php
+++ config/keys.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Config-file for API KEYS.
+ * Config-file for Ip & Weather app.
  */
 return [
-    "darkskyKey" => "68d95de47fff4f718c01ead152cab09e",
-    "ipKey" => "59f40c392b861e29e674546a49e37b53",
+    "darksky" => "68d95de47fff4f718c01ead152cab09e",
+    "ip" => "59f40c392b861e29e674546a49e37b53",
 ];
